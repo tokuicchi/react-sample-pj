7c91101fa83deb2b064d81287f7d65f0219e0bf3
徳井 雄介 <tokui@cloudpack.jp>
2024-03-22 16:51:10 +0900

    fix: typo修正

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -1,12 +1,12 @@
-import { INCREMENT, DECREMENT } from '../actions'
+import { increment, decrement } from '../actions'
 
 const initialState = { value: 0 }
 
 export default (state = initialState, action) => {
     switch (action.type) {
-        case INCREMENT:
+        case increment:
             return { value: state.value + 1 }
-        case DECREMENT:
+        case decrement:
             return { value: state.value - 1 }
         default:
             return state
